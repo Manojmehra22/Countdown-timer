e6e6af8643e8a88bcd6e491e208345089f039920
Manojmehra22 <manojmehr650@gmail.com>
2025-01-11 23:29:40 +0530

    updates changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import CounterTimer from "./Components/CounterTimer";
+
+const App = () => {
+  return (
+    <div>
+      <CounterTimer />
+    </div>
+  );
+};
+
+export default App;
